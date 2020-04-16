8cdae8ec5026b6077ef10a57e7e122d8a266dbf2
cmruss <cmrussell08@gmail.com>
2020-04-16 12:09:31 -0400

    commiting progress before Brady reveals the solution

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -8,20 +8,19 @@ import sys
 def eating_cookies(n, cache=None):
   if cache is None:
     cache = {}
-  if n < 1:
-    return 0
   if n < 2:
     return 1
-  elif n in cache:
+  elif n in cache and cache[n] > 0:
+    # cache = {i: cache[i] for i in range(0, len(cache))}
     return cache[n]
-  elif n < 4 : 
+  elif n < 4: 
     ways = 2 * eating_cookies(n-1, cache)
-    # print(ways)
+    print(ways)
     cache[n] = ways
     return ways
   else:
     ways = eating_cookies(n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache)
-    # print(ways)
+    print(ways)
     cache[n] = ways
     return ways
 
